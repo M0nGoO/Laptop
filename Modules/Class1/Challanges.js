4c4b12dd2ba8780307545ab50aab788d57c825e0
Erblin Gara <garaerblin@gmail.com>
2024-02-13 19:41:34 -0500

    Challenges

---
+++ Modules/Class1/Challanges.js
@@ -0,0 +1,21 @@
+let fullName = 'Erblin Gara';
+console.log(`My name is ${fullName}`);
+
+let age = 27;
+console.log(`I am ${age} years old`);
+
+let isStudent = true;
+console.log(typeof isStudent);
+
+let favouriteColors = ['red', 'green', 'blue'];
+console.log(`My favourite colors are ${favouriteColors}`);
+
+let person = {
+    firstName : 'Erblin',
+    lastName : 'Gara',
+    age : 27,
+    Gender : 'M'
+};
+
+console.log(person);
+
